70601cf03ed2ea76efd054bcec282a41baa86c0d
baolengoc <lengocbaovlth1513010@gmail.com>
2024-06-13 17:46:56 +0700

    update

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,25 +3,30 @@ import Filter from "@/components/filter";
 import Sort from "@/components/filter/Sort";
 import ProductList from "@/components/product/ProductList";
 import { fetchProducts } from "@/services/products";
-const LIMIT_PRODUCTS = 12;
+import { Suspense } from "react";
 export default async function ShopPage() {
   const productResponse = await fetchProducts({
-    page: Number(1),
-    take: LIMIT_PRODUCTS,
+    page: 1,
+    take: 8,
   });
 
   return (
     <div className="container py-20 grid grid-cols-1 sm:grid-cols-4 gap-10">
-      <Filter />
+      <Suspense fallback={<div></div>}>
+        <Filter />
+      </Suspense>
+
       <div className="col-span-3 space-y-4 flex flex-col items-center">
         <div className="w-full flex justify-end">
           <Sort />
         </div>
         <ProductList products={productResponse.data} />
-        <AppPagination
-          totalPages={Number(productResponse.meta.pageCount)}
-          page={Number(productResponse.meta.page)}
-        />
+        <Suspense fallback={<div></div>}>
+          <AppPagination
+            totalPages={Number(productResponse.meta.pageCount)}
+            page={Number(productResponse.meta.page)}
+          />
+        </Suspense>
       </div>
     </div>
   );
